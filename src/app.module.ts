0294ec38e71218ad5b09fee14b9dae6bda040dc4
inzahan <inzahanyang@gmail.com>
2021-12-29 02:09:22 +0900

    main_change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { PodcastsController } from './podcasts/podcasts.controller';
 import { PodcastsService } from './podcasts/podcasts.service';
 
 @Module({
   imports: [],
-  controllers: [PodcastsController],
+  controllers: [PodcastsController, AppController],
   providers: [PodcastsService],
 })
 export class AppModule {}
